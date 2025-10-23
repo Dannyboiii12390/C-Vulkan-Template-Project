56bf132a7ec8ca31e227d7e455c58ed2ef2bb788
DANIEL HOWE <Djhowe18@gmail.com>
2025-10-23 15:20:08 +0100

    changed file properties of shaders to be part of the build process

--- C++ Vulkan Template Project/Core/UBO.h
+++
@@ -1,9 +0,0 @@
-
-#include <glm/glm.hpp>
-
-
-struct UniformBufferObject {
-    alignas(16) glm::mat4 model;
-    alignas(16) glm::mat4 view;
-    alignas(16) glm::mat4 proj;
-};
--- C++ Vulkan Template Project/SHADERS/compile.bat
+++
@@ -1,3 +0,0 @@
-C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shader.vert -o shader.vert.spv
-C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shader.frag -o shader.frag.spv
-pause
